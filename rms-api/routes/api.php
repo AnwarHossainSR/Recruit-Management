ff1bc4cfaf590ad865c869f1fa15ed030e2af6a2
AnwarHossainSR <mahedianwar@gmail.com>
2021-12-04 10:31:11 +0600

    implement JWT authentication

--- rms-api/routes/api.php
+++ rms-api/routes/api.php
@@ -1,19 +1,17 @@
 <?php
 
+use App\Http\Controllers\auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
 });
