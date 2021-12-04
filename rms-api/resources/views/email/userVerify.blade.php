d566e3dc9eaa44170f526b1026d5b6701fa5e5d8
AnwarHossainSR <mahedianwar@gmail.com>
2021-12-05 02:19:15 +0600

    implement verify feature part 1

---
+++ rms-api/resources/views/email/userVerify.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Verify Email</title>
+</head>
+<body>
+    <h2>Dear <span>{{$details['name']}}</span></h2>
+    <p>We are requesting you to verify your email address Please clink bellow link to verify your account</p>
+    <a href="http://127.0.0.1:8000/verify/{{$details['token']}}/{{$details['email']}}">Verify Here</a>
+    <br><br><br>
+    <p>Thank You</p>
+</body>
+</html>
