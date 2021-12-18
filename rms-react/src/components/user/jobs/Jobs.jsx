dca9a0690a366187af894d8af93b33e49aa82158
AnwarHossainSR <mahedianwar@gmail.com>
2021-12-18 23:37:52 +0600

    jobs section design

--- rms-react/src/components/user/jobs/Jobs.jsx
+++ rms-react/src/components/user/jobs/Jobs.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import JobItem from "./JobItem";
 
 const Jobs = () => {
-  return <div>Jobs</div>;
+  return (
+    <div className="job">
+      <div className="container">
+        <div className="job-info">
+          <h1>Latest Jobs</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus
+            animi unde architecto magni illo quis id illum aperiam magnam rem.
+          </p>
+        </div>
+        <div className="job__wrapper">
+          <JobItem />
+          <JobItem />
+          <JobItem />
+          <JobItem />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Jobs;
