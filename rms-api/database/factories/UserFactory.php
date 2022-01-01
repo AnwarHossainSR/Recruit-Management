9c173d1fd9a987e2fafbce1eb362653468c92a85
AnwarHossainSR <mahedianwar@gmail.com>
2022-01-01 19:33:18 +0600

    homepage job section

--- rms-api/database/factories/UserFactory.php
+++ rms-api/database/factories/UserFactory.php
@@ -20,7 +20,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'is_admin' => false,
-            'image'=>'default.png',
+            'image'=>'http://localhost:8000/files/users/default.png',
         ];
     }
 }
